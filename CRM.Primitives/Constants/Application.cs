79ff648e540239fb6ce5cefdf6dd5541569095f9
Luke Alderton <lukealderton@outlook.com>
2026-08-15 16:23:59 +0100

    Updated titles

---
+++ CRM.Primitives/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace CRM.Primitives.Constants
+{
+    public static class Application
+    {
+        public const String ApplicationName = "CRM";
+        public const String ApplicationAddress = "https://hall.alderton.org.uk/";
+    }
+}
